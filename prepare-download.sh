18d00ca96370224e2225e17552b598c5f406a2c8
Zetaphor <zetaphor@hey.com>
2024-06-06 18:25:22 -0500

    Add zip script

---
+++ prepare-download.sh
@@ -0,0 +1,16 @@
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <folder_path>"
+    exit 1
+fi
+
+folder_to_zip=$1
+
+zip_file_name="$(basename "$folder_to_zip").zip"
+
+zip -r "$zip_file_name" "$folder_to_zip" -x "*node_modules*/*"
+
+mkdir -p downloads
+
+mv "$zip_file_name" downloads/
+
+echo "$zip_file_name zipped successfully"
